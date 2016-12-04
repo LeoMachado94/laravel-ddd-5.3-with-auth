7601c069659c7bf84febb4389d5358caf0319cc8
Leonardo Machado <leo94.machado@gmail.com>
2016-12-04 19:28:16 +0000

    Laravel 5.3 with Authentication

---
+++ app/Applications/User/Providers/UserServiceProvider.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Applications\User\Providers;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+
+class UserServiceProvider extends ServiceProvider
+{
+    protected $namespace = 'App\Applications\User\Http\Controllers';
+
+    /**
+     * Define your route model bindings, pattern filters, etc.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        parent::boot();
+    }
+
+    /**
+     * Define the routes for the application.
+     *
+     * @return void
+     */
+    public function map()
+    {
+        $this->mapWebRoutes();
+    }
+
+    /**
+     * Define the "web" routes for the application.
+     *
+     * These routes all receive session state, CSRF protection, etc.
+     *
+     * @return void
+     */
+    protected function mapWebRoutes()
+    {
+        Route::group([
+            'middleware' => 'web',
+            'namespace' => $this->namespace,
+        ], function ($router) {
+            require base_path('routes/applications/user.php');
+        });
+    }
+}
